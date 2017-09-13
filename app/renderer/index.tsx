dbd801e5364167ec6d17b5a7864fed2806d87396
suttang <suttang@gmail.com>
2017-09-13 20:29:09 +0900

    全ファイル削除

--- app/renderer/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { App } from './App'
-
-const container = document.getElementById('app');
-
-ReactDOM.render(<App />, container)
